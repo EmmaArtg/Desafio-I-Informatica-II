009eb3eee36c35a3c97be60e48ed0e6ff4991a51
KhanBoy <99689752+EmanuelVelz03@users.noreply.github.com>
2026-03-17 11:38:20 -0500

    Tablero juego Tetris actualizado

---
+++ main/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_main_FILE C:/Users/User/Documents/main/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/main.exe)
+set(__QT_DEPLOY_TARGET_main_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_main_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
