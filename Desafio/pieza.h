5d7a538f93994f9a80269a3fd8532a339b561419
KhanBoy <99689752+EmanuelVelz03@users.noreply.github.com>
2026-03-19 17:45:49 -0500

    Repositorio actualizado eliminación de archivos

---
+++ Desafio/pieza.h
@@ -0,0 +1,16 @@
+#ifndef PIEZA_H
+#define PIEZA_H
+#include <cstdint>
+
+struct piezaactiva {
+    int id;
+    int rotacion;
+    int x;
+    int y;
+};
+
+extern const uint8_t piezas[7][4][4];
+extern const int alturapiezas[7][4];
+extern const int anchopiezas[7][4];
+
+#endif
