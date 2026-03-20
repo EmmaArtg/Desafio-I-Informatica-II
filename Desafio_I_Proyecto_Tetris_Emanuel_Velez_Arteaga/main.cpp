7fe211bb5f405db63a2b155e73b027b6302e02ff
KhanBoy <99689752+EmanuelVelz03@users.noreply.github.com>
2026-03-20 00:46:04 -0500

    Main actualizado

--- Desafio_I_Proyecto_Tetris_Emanuel_Velez_Arteaga/main.cpp
+++ Desafio_I_Proyecto_Tetris_Emanuel_Velez_Arteaga/main.cpp
@@ -32,6 +32,65 @@ int main() {
             tablero[i][j] = 0;
         }
     }
+    srand((unsigned)time(0));
+    bool jugando = true;
+
+    while (jugando) {
+        PiezaActiva actual;
+        actual.id       = rand() % 7;
+        actual.rotacion = 0;
+        actual.x        = (ancho / 2) - 2;
+        actual.y        = 0;
+
+        if (!esMovimientoValido(tablero, alto, ancho, actual, actual.x, actual.y, actual.rotacion)) {
+            imprimirTablero(tablero, alto, ancho, actual);
+            cout << "\nGAME OVER\n";
+            break;
+        }
+
+        bool piezaActiva = true;
+        while (piezaActiva) {
+            imprimirTablero(tablero, alto, ancho, actual);
+
+            char accion;
+            cin >> accion;
+
+            switch (accion) {
+            case 'a': case 'A':
+                if (esMovimientoValido(tablero, alto, ancho, actual, actual.x - 1, actual.y, actual.rotacion))
+                    actual.x--;
+                break;
+            case 'd': case 'D':
+                if (esMovimientoValido(tablero, alto, ancho, actual, actual.x + 1, actual.y, actual.rotacion))
+                    actual.x++;
+                break;
+            case 's': case 'S':
+                if (esMovimientoValido(tablero, alto, ancho, actual, actual.x, actual.y + 1, actual.rotacion)) {
+                    actual.y++;
+                } else {
+                    fijarPieza(tablero, ancho, actual);
+                    limpiarFilas(tablero, alto, ancho);
+                    piezaActiva = false;
+                }
+                break;
+            case 'w': case 'W': // Rotar
+            {
+                int nuevaRot = (actual.rotacion + 1) % 4;
+                if (esMovimientoValido(tablero, alto, ancho, actual, actual.x, actual.y, nuevaRot))
+                    actual.rotacion = nuevaRot;
+            }
+            break;
+            case 'q': case 'Q':
+                piezaActiva = false;
+                jugando = false;
+                break;
+            default:
+                break;
+            }
+        }
+    }
+
+
 
     for (int i = 0; i < alto; i++) {
         delete[] tablero[i];
